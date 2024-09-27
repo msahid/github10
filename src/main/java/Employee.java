e746606bfd1ceb5de7962422cd3f6749862b01dc
Sahid <mscode298@gmail.com>
2024-09-26 23:34:36 -0400

    Implement Employee Interface

---
+++ src/main/java/Employee.java
@@ -0,0 +1,32 @@
+public class Employee {
+
+    private int empID;
+    private String empName;
+
+    public int getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public int getEmpSalary() {
+        return empSalary;
+    }
+
+    public void setEmpSalary(int empSalary) {
+        this.empSalary = empSalary;
+    }
+
+    private  int empSalary;
+
+}
---
+++ src/main/java/EmployeeDAO.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public interface EmployeeDAO {
+    public void saveEmployee (Employee emp);
+    public  void updateEmployee(int empId, int empSalary);
+    public void deleteEmployee (int empId);
+    public List<Employee> listEmployees();
+}
