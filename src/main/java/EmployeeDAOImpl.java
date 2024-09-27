ff70e7a6d31b8587c039b506293ad9e43c90eb9a
Sahid <mscode298@gmail.com>
2024-09-26 23:40:04 -0400

    Employee Interface added

---
+++ src/main/java/EmployeeDAOImpl.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class EmployeeDAOImpl implements EmployeeDAO {
+    @Override
+    public void saveEmployee(Employee emp) {
+
+    }
+
+    @Override
+    public void updateEmployee(int empId, int empSalary) {
+
+    }
+
+    @Override
+    public void deleteEmployee(int empId) {
+
+    }
+
+    @Override
+    public List<Employee> listEmployees() {
+        return null;
+    }
+}
